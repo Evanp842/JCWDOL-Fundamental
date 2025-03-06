67e5c5b200381f9fe83ed23f5c0bef399ac7ba4e
Evan <aevan842@gmail.com>
2025-03-06 22:19:18 +0700

    Day 4

---
+++ Day4/array.ts
@@ -0,0 +1,151 @@
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f", "g");
+// console.log(arr);
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// arr.unshift("z");
+// console.log(arr)
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// arr.unshift("z");
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// arr.unshift("z");
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// arr.unshift("z");
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+// arr.splice(2, 2)
+// console.log(arr)
+
+// const arr: string[] = ["a","b","c","d","e"];
+// const arr2: string[] = new Array("a","b","c","d","e");
+
+// console.log(arr);
+// console.log(arr2);
+
+// console.log(arr[2])
+
+// arr.push("f");
+// console.log(arr);
+
+// arr.unshift("z");
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+// arr.splice(2,2, "m", "n");
+// console.log(arr)
+
+const arr: string[] = ["a","b","c","d","e"];
+const arr2: string[] = new Array("a","b","c","d","e");
+
+console.log(arr);
+console.log(arr2);
+
+console.log(arr[2])
+
+arr.push("f");
+console.log(arr);
+
+arr.unshift("z");
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+arr.splice(2,2, "m", "n");
+console.log(arr)
+
+const numArr: number[] = [10, 12, 20, 42, 100];
+
+numArr.sort((a, b) => a - b);
+console.log(numArr);
+
+const newArr: number[] = numArr.filter((item) => item <= 30);
+console.log(newArr)
+
+console.log(arr.reverse());
--- first.ts
+++
@@ -1,7 +0,0 @@
-let name : string = "Budi";
-
-console.log(name)
-console.log(name.toUpperCase())
-console.log(name.toLowerCase())
-console.log(name.replace("i", "o"))
-console.log(name.slice(0,2))
